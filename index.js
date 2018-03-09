ecdf8c8d0cf9d51c283e0d196b2f6817a530723d
Manuel Valles <manukempo@gmail.com>
2018-03-09 13:48:45 +0000

    Passport.js & Google OAuth Strategy

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 // Import Express library
 const express = require('express');
+// Import Passport modules (We just need the Strategy property)
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// Import keys
+const keys = require('./config/keys');
 // Create Express app
 const app = express();
 
-// Route handler
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+
+// Apply a new strategy
+passport.use(new GoogleStrategy(
+  {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log('access Token', accessToken);
+    console.log('refresh Token', refreshToken);
+    console.log('Profile', profile);
+  }
+));
+
+// Route Handler for Auth
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+// Route for auth/google/callback
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 // Bind PORT for Heroku (Dynamic) OR local environment 
 const PORT = process.env.PORT || 5000;
